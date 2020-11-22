85eba90af59df155b1dabc12511858ff4ddae0d5
GSLAB\GSC-30542 <vishal.kumbhar@gslab.com>
2020-11-22 23:57:27 +0530

    working code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {  useContext } from "react";
 import './App.css';
-
+import Headers from './components/Header/Headers';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import ProductDetails from './components/Products/ProductDetails';
+import { CartContext } from "./reducer/Context";
+import CartItems from './components/CartItems/CartItems';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+      <Headers />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/product-details/:id" component={ProductDetails} />
+       <Route exact path="/cart" component={CartItems} />
+       <Route exact path="/place-order" component={PlaceOrder} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
